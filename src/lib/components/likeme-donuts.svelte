58d5905e422a749e4ab428dcc9e847e7653c3e6c
Moechinehaa <jessica.ready.win@gmail.com>
2024-11-19 17:35:44 -0700

    likemedonuts with mouseenter and mouseleave

--- src/lib/components/likeme-donuts.svelte
+++ src/lib/components/likeme-donuts.svelte
@@ -7,17 +7,26 @@
     let arc_ ,root,colorScale
 
     const INRtoCat = [
-        { min: 0, max: 50, category: "A" },
-        { min: 51, max: 100, category: "B" },
-        { min: 101, max: 150, category: "C" },
-        { min: 151, max: 500, category: "D" }
-    ];
+            { min: -1, max: 50, category: "A" },
+            { min: 50, max: 100, category: "B" },
+            { min: 100, max: 150, category: "C" },
+            { min: 150, max: 500, category: "D" }
+    ]
+
+    const AgeGrouptoCat = {
+        "under 18" : 0,
+        "18-25"    : 1,
+        "25-45"    : 2,
+        "45-60"    : 3,
+        "60 and above" : 4
+    }
+
 
     $effect(async()=>{
         await loadData()
         buildHierarchy()
         drawDonuts()
-        //console.log(temp_data)
+        //console.log('A'.charCodeAt(0) - 'B'.charCodeAt(0))
     })
 
     async function loadData() {
@@ -67,25 +76,63 @@
             .innerRadius(function(d) { return Math.max(55, Math.sqrt(d.y0)); })
             .outerRadius(function(d) { return Math.max(55, Math.sqrt(d.y1)); })
         
-        colorScale = scaleOrdinal(schemeCategory10);
+        colorScale = scaleOrdinal()
+		.domain(['exact', 'half-smi', 'non-smi'])
+		.range(['#02576c', '#E8DB64', 'gray'])
+    }
+
+    function SimilaritytoCat(smi){
+        if(smi == 0)return "exact";
+        else if(smi == 1)return "half-smi";
+        else{
+            return "non-smi";
+        }
     }
 
 
     function drawDonuts(){
 
-        select("#likeme-donuts")
+        const donuts = select("#likeme-donuts")
             .append("g")
             .attr('transform', 'translate(' + 190 + ',' + 120 + ')')
+        donuts
             .selectAll("path")
             .data(root.descendants())
             .enter().append("path")
             .attr("d", arc_)
             .style("fill", function(d){
-                console.log("this",d)
-                return colorScale(d.name)
+                //console.log(d)
+                var temp
+                if(d.depth == 1){ //age group
+                    temp = SimilaritytoCat(Math.abs(AgeGrouptoCat[d.data.name]-AgeGrouptoCat[temp_user[0]]))
+                }
+                else if(d.depth == 2){ //gender
+                    if(d.data.name == temp_user[1]){
+                        temp = "exact"
+                    }
+                    else{
+                        temp = "non-smi"
+                    }
+                }
+                else if(d.depth == 3){ //inr
+                    //console.log("check",d.data.name.charCodeAt(0),temp_user[2].charCodeAt(0))
+                    temp = SimilaritytoCat(Math.abs(d.data.name.charCodeAt(0)-temp_user[2].charCodeAt(0)))
+                }
+                else{
+                    temp = "non-smi"
+                }
+                return colorScale(temp)
             })
             .style("stroke", "white")
             .style("stroke-width", "1px")
+            .on("mouseenter",function(event,d){
+                const ancestors = d.ancestors().reverse().slice(1) //remove root(the first element)
+                donuts.selectAll("path")
+                    .style("opacity", node => ancestors.includes(node) ? 1 : 0.7);
+            })
+            .on("mouseleave",function(event,d){
+                donuts.selectAll("path").style("opacity", 1);
+            })
     }
 
 </script>

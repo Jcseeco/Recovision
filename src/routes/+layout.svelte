6cdbb99756698dac9179e5a053a4bd6a876d8774
Jcseeco <dontbirdanyone@gmail.com>
2024-12-06 17:37:05 -0700

    fix: page link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import { page } from '$app/stores';
 	import Logo from '$lib/components/logo.svelte';
+	import { systemData } from '../lib/utils/storage.svelte';
 	let { children } = $props();
 </script>
 
@@ -60,27 +61,13 @@
 					Data config</a
 				>
 			</li>
-			<li>
-				<a href="/recommand" class={$page.url.pathname === '/recommand' && 'active'}>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="size-5"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"
-						/>
-					</svg>
-					Review Recommandation</a
+			<li title={systemData.seedCustomer ? '' : 'selected a seed customer first!'}>
+				<a
+					href="/similars"
+					class="{$page.url.pathname === '/similars' && 'active'} {systemData.seedCustomer
+						? ''
+						: 'disabled'}"
 				>
-			</li>
-			<li>
-				<a href="/similars" class={$page.url.pathname === '/similars' && 'active'}>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
@@ -96,6 +83,30 @@
 						/>
 					</svg>
 					Review Similar Records</a
+				>
+			</li>
+			<li title={systemData.seedCustomer ? '' : 'selected a seed customer first!'}>
+				<a
+					href="/recommand"
+					class="{$page.url.pathname === '/recommand' && 'active'} {systemData.seedCustomer
+						? ''
+						: 'disabled'}"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-5"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"
+						/>
+					</svg>
+					Review Recommandation</a
 				>
 			</li>
 			<li class="mt-auto">
@@ -131,3 +142,11 @@
 		</ul>
 	</div>
 </div>
+
+<style>
+	a.disabled {
+		pointer-events: none;
+		cursor: not-allowed;
+		background-color: oklch(var(--b3));
+	}
+</style>
